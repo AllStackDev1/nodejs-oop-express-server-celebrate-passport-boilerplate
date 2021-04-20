230cae0ea980057d91193ac7999babf30bc21a1f
Chinedu Ekene Okpala <allstackdev@gmail.com>
2021-04-20 19:26:47 +0000

    update docker file

--- src/tests/init.js
+++ src/tests/init.js
@@ -1,6 +1,6 @@
 /**
  * Test initialization file containing required packages for most tests
- * @author Chinedu Ekene Okpala <chinedu.okpala@completefarmer.com>
+ * @author Chinedu Ekene Okpala <allstack@gmail.com>
  */
 
 // set environment to test
